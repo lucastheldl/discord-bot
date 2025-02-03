a0c6d29a96b9e6aaf84d50ac812ebccc32ee4e4d
lucas Emanoel <lucastheldl@gmail.com>
2025-02-03 00:06:08 -0300

    feature/added-utils

---
+++ src/lib/ships-imgs.js
@@ -0,0 +1,8 @@
+const shipsSuper = [
+	"https://i.pinimg.com/736x/ec/59/c1/ec59c123550b06154ede21c5ed51e9e2.jpg",
+	"https://i.pinimg.com/736x/db/57/05/db57054b37346b78318cd5435f22a21b.jpg",
+	"https://i.pinimg.com/736x/72/f5/05/72f505c958f66240cdff97af050b4a19.jpg",
+	"https://i.pinimg.com/736x/f2/8d/8e/f28d8e91d2821e43fb7c432dfce29154.jpg",
+];
+
+module.exports = { shipsImages };
