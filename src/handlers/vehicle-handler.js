9f78e062c9c9d603c705255e3a364d9300709f82
lucas Emanoel <lucastheldl@gmail.com>
2025-02-10 01:54:00 -0300

    feature/added-btns-to-vehicle-interaction

--- src/handlers/vehicle-handler.js
+++ src/handlers/vehicle-handler.js
@@ -6,11 +6,33 @@ const {
 	MessageFlags,
 	SlashCommandBuilder,
 } = require("discord.js");
+const options = [
+	{ id: "1", name: "Tentar roubar" },
+	{ id: "2", name: "Atacar" },
+];
 async function interactWithFoundVehicle(buttonInteraction, veicles) {
 	const veiclesId = buttonInteraction.customId.split("_")[1];
 	const veicle = veicles.find((p) => p.id === Number.parseInt(veiclesId));
 
 	if (!veicle) return;
+
+	const rows = [];
+	let currentRow = new ActionRowBuilder();
+
+	options.forEach((opt, index) => {
+		const button = new ButtonBuilder()
+			.setCustomId(opt.id)
+			.setLabel(opt.name)
+			.setStyle(ButtonStyle.Primary);
+
+		currentRow.addComponents(button);
+
+		// Discord allows max 5 buttons per row
+		if (currentRow.components.length === 5 || index === options.length - 1) {
+			rows.push(currentRow);
+			currentRow = new ActionRowBuilder();
+		}
+	});
 
 	const resultEmbed = new EmbedBuilder()
 		.setColor(0x00ff00)
@@ -20,6 +42,7 @@ async function interactWithFoundVehicle(buttonInteraction, veicles) {
 
 	await buttonInteraction.reply({
 		embeds: [resultEmbed],
+		components: rows,
 	});
 }
 
